25a7b8eb48386b880b496a585d56229cf5984b1b
Lauren Morgan <rg326@live.com>
2022-05-19 20:41:55 -0400

    initial commit, folder set up, create react app and server

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+
+//USE
+app.use('/',(req, res)=>{
+    
+})
+
+//GET
+app.get('/')
+
+//POST
+app.post('/')
+//PATCH
+
+//DELETE
+
+//RUN
+app.listen(3000, ()=>{
+    console.log('Server running on port 3000')
+})
