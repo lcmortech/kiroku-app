561bcb1fbd375781e90dd128ce39eca4849f200c
Lauren Morgan <rg326@live.com>
2022-05-20 17:36:03 -0400

    added useeffect hook to fetch data

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import {useState, useeffect} from 'reacts'
+import {useState, useEffect} from 'reacts'
 
 
 function App() {
 const [title, setTitle] = useState('')
 const [id, setId] = useState(0)
+
+useEffect(async()=>{
+  const res = await fetch(``)
+  const data = await res.json()
+
+  data.slice(0,4)
+},[])
 
   return (
     <div className="App">
